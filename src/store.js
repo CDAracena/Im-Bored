a9b9821f54bdd3e004406729cfbdde01f597b49a
Christian Aracena <CDAracena@gmail.com>
2018-12-09 02:29:15 -0500

    updating redux store with api data

--- src/store.js
+++ src/store.js
@@ -1,9 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducer from './reducers/reducer';
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE || compose;
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store;
