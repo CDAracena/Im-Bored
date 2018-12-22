ce5c73ec166976c09dbf3939bb45d2d3e09c768c
Christian Aracena <CDAracena@gmail.com>
2018-12-22 03:16:17 -0500

    added filter choices

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -10,10 +10,16 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
-import {closeDrawer, deleteFromFavorites} from '../actions/actions';
+import {closeDrawer, deleteFromFavorites, addToFavorites} from '../actions/actions';
 import Delete from '@material-ui/icons/Delete';
+import Favorite from '@material-ui/icons/Favorite'
 import IconButton from '@material-ui/core/IconButton';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import {Education, BikeRide, Social, Diy, Charity, Cook, Spa, Music, Work} from '../utils/svg_icons';
+
 
 const styles = (theme) => ({
   list: {
@@ -32,6 +38,19 @@ const styles = (theme) => ({
 });
 
 class LeftDrawer extends React.Component {
+  state = {
+    filterChoices:[
+{type: 'education', icon: Education},
+{type: 'recreational', icon: BikeRide},
+{type:'social', icon: Social},
+{type: 'diy', icon: Diy},
+{type: 'charity', icon:Charity},
+{type: 'cooking', icon: Cook},
+{type: 'relaxation', icon: Spa},
+{type: 'music', icon: Music},
+{type: 'busywork', icon: Work}],
+
+      }
 
   renderSideListType = (type) => {
     switch (type) {
@@ -42,18 +61,25 @@ class LeftDrawer extends React.Component {
     }
   }
 
+  filterChoice = (choice) => {
+    console.log(choice)
+  }
+
   render() {
-    const {classes, drawerOpen, drawerType, deleteItem} = this.props;
+    const {classes, drawerOpen, drawerType, deleteItem, addToFavorites} = this.props;
     const sideList = (<div className={classes.drawer}>
       <List>
         {
           drawerType && this.renderSideListType(drawerType).map((item, index) => (<ListItem button="button" key={index}>
             <ListItemText primary={item.activity}/>
-          {  drawerType === 'favorites' && <ListItemSecondaryAction>
-              <IconButton onClick={()=> deleteItem(item)} className={classes.trashCan}>
-                <Delete/>
+            <ListItemSecondaryAction>
+              <IconButton onClick={ drawerType === 'favorites' ? ()=> deleteItem(item) : ()=> addToFavorites(item)} className={classes.trashCan}>
+              {drawerType === 'favorites' ?  <Delete/> : <Favorite/>}
               </IconButton>
-            </ListItemSecondaryAction>}
+            </ListItemSecondaryAction>
+            <List>
+              {item.link && <ListItem button component="a" href={`${item.link}`}> <ListItemText primary={item.link}/> </ListItem>}
+            </List>
           </ListItem>))
         }
       </List>
@@ -63,6 +89,12 @@ class LeftDrawer extends React.Component {
     return (<div>
       <Drawer open={drawerOpen} onClose={this.props.closeLeftDrawer}>
         <div>
+          <Grid container justify='center' alignItems='center'>
+            <Typography color="primary"> Filter </Typography>
+                {this.state.filterChoices.map((choice, idx) => <Grid>
+                  <IconButton color="primary" onClick={() => this.filterChoice(choice.type)}> <SvgIcon> <path d={choice.icon}/> </SvgIcon> </IconButton>
+                 </Grid> )}
+          </Grid>
           {sideList}
         </div>
       </Drawer>
@@ -77,7 +109,9 @@ const mapStateToProps = ({apiData, favorites, history, drawerOpen, drawerType}) 
 const mapDispatchToProps = dispatch => {
   return {
     closeLeftDrawer: () => dispatch(closeDrawer()),
-    deleteItem: (item) => dispatch(deleteFromFavorites(item))
+    deleteItem: (item) => dispatch(deleteFromFavorites(item)),
+    addToFavorites: (data) => dispatch(addToFavorites(data))
+
   }
 }
 
