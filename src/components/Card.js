117497f3115959d7ca319780c5094299883cb07f
CDAracena <cdaracena@gmail.com>
2019-02-22 22:43:49 -0500

    added search inputs for dad and advice cards, passing test

--- src/components/Card.js
+++ src/components/Card.js
@@ -20,6 +20,7 @@ import WbSunny from '@material-ui/icons/WbSunny';
 import AccessibilityNew from '@material-ui/icons/AccessibilityNew';
 import LocationCity from '@material-ui/icons/LocationCity';
 import Face from '@material-ui/icons/Face';
+import Input from '@material-ui/core/Input';
 import {fetchGeekJoke, fetchDadJoke, postDadJoke, fetchCorporateBS, fetchAdvice} from '../../src/utils/api'
 
 // Maybe add favorites to the bottom / collapse area of the card.
@@ -74,6 +75,13 @@ class JokesterCard extends Component {
     }
   }
 
+renderInput = (title) => {
+  if (title === 'Dad Joke' || title === 'Advice') {
+    return (
+      <Input data-cy="card-search-input"/>
+    )
+  }
+}
 
   render() {
     const { classes, cardTitle, cardSubheader, apiChoice, searchable} = this.props;
@@ -104,6 +112,7 @@ class JokesterCard extends Component {
           <Typography component="p">
             {this.state.joke}
           </Typography>
+          {this.renderInput(cardTitle)}
         </CardContent>
         <CardActions className={classes.actions} disableActionSpacing>
           <IconButton aria-label="Add to favorites">
@@ -125,29 +134,7 @@ class JokesterCard extends Component {
         </CardActions>
         <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
           <CardContent>
-            <Typography paragraph>Method:</Typography>
-            <Typography paragraph>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-              minutes.
-            </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-              heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-              browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-              chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-              salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-              minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-            <Typography paragraph>
-              Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-              without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat
-              to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and
-              cook again without stirring, until mussels have opened and rice is just tender, 5 to 7
-              minutes more. (Discard any mussels that don’t open.)
-            </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then serve.
-            </Typography>
+
           </CardContent>
         </Collapse>
       </Card>
