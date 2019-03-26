80770e73f9dd1e657c735c7d37b61e4a6f744a38
CDAracena <cdaracena@gmail.com>
2019-03-26 03:18:48 -0400

    added token reducer to combined rootReducer

--- src/store.js
+++ src/store.js
@@ -4,11 +4,13 @@ import thunk from 'redux-thunk';
 import core from './reducers/core';
 import suggestion from './reducers/suggestions'
 import jokester from './reducers/jokester'
+import token from './reducers/token';
 
 const rootReducer = combineReducers({
   core,
   suggestion,
-  jokester
+  jokester,
+  token
 })
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
