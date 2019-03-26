14f590a25a6f3ac69f8190cd80e4f603de313b5e
CDAracena <cdaracena@gmail.com>
2019-03-26 03:14:26 -0400

    added token reducer initial state

---
+++ src/reducers/token.js
@@ -0,0 +1,15 @@
+const initialState = {
+  validating: false,
+  isValidated: false,
+  accessToken: '',
+  uid: '',
+  client: '',
+  currentUser: {}
+}
+
+const token = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+    return state;
+  }
+}
